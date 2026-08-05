428f8a0dec758855516fb971aafe56eeeb17bbd7
DareenHazem <dareen.hazem2005@gmail.com>
2026-08-05 16:20:51 +0300

    Assignment 01 Code

--- OOP02/Program.cs
+++ OOP02/Program.cs
@@ -4,7 +4,65 @@
     {
         static void Main(string[] args)
         {
-           
+            #region Part 01 : Theoretical Questions
+
+            #region Question 01
+            //public struct DeliveryAddress { 
+            //      public string City;
+            //      public string Street;
+            //}
+            //public class Customer {
+            //      public string Name;
+            //}
+            // a) What happens when a DeliveryAddress variable is copied into another variable and the copy is modified?
+            // b) [What happens when a Customer variable is copied into another variable and one variable modifies the object
+
+            // 1- Struct is a value type so there won't be change in the original value cuz the variable only have copy of the original value not access to it
+            // 2- Class is a refernce type and we copy the address so they refere to the same object in heap so any it will change the original value
+            #endregion
+
+            #region Question 02
+            /*public struct Shipment{
+               public string Description; 
+               public double Weight; 
+               public decimal DeliveryFee;
+               } 
+             a) Identify at least three problems with this design from an encapsulation perspective.
+             b) How can private fields and public properties improve this design?
+             */
+
+
+            // 1-  (1) Anyone can access the fields directly and modify it
+            //     (2) There is no validations so it will icrease bugs
+            //     (3) Any change in the class code will affect the other files 
+
+            // 2-  (1) Not everyone will have access diectly to the fields so we will protect data
+            //     (2) It will reduce the bugs cuz there will be validations
+            //     (3) We can change only in the class file without affecting other code so it will reduce errors
+            #endregion
+
+            #endregion
+
+
+            #region Part 02 : Practical
+
+            #region  DeliveryAddress - Quesetion01
+            // Create one DeliveryAddress value, copy it into a second variable, modify the copy,
+            // and print both values to prove that the original did not change.
+
+            //DeliveryAddress delivery01 = new("Cairo", "5 street", 10);
+            //DeliveryAddress delivery02 = delivery01;
+            //delivery02.city = "Giza";
+            //delivery02.street = "7 street";
+            //delivery02.buildingNumber = 3;
+            //Console.WriteLine(delivery01.GetFullAddress());
+            //Console.WriteLine(delivery02.GetFullAddress()); 
+            #endregion
+
+
+
+            #endregion
+
 
         }
     }
---
+++ OOP02/Shipment.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP02
+{
+    internal struct Shipment
+    {
+        // Attributes
+        string trackingCode;
+        string description;
+        double weight;
+        decimal deliveryFee;
+
+        #region Properties
+        public DeliveryAddress Destination
+        {
+            get; set;
+        }
+
+        public string TrackingCode
+        {
+            get { return trackingCode; }
+        }
+
+        public string Description
+        {
+            get { return description; }
+            set { description = !string.IsNullOrWhiteSpace(value) ? value : description; }
+        }
+
+        public double Weight
+        {
+            get { return weight; }
+            set { weight = value > 0 ? value : weight; }
+        }
+        public decimal DeliveryFee
+        {
+            get { return deliveryFee; }
+            private set { deliveryFee = value; }
+        }
+        public decimal EstimatedCost
+        {
+            get { return DeliveryFee + (decimal)(Weight * 5); }
+        }
+        #endregion
+
+        #region Constructor 
+        public Shipment(string trackingCode)
+        {
+            this.trackingCode = !string.IsNullOrWhiteSpace(trackingCode) ? trackingCode : "Unknown";
+            description = "Unknown";
+            weight = 1;
+            deliveryFee = 50;
+            Destination = new DeliveryAddress("Unknown", "Unknown", 0); // or Destination = default;
+
+        }
+
+        public Shipment(string trackingCode, string description, double weight, decimal deliveryFee, DeliveryAddress destination)
+        {
+            this.trackingCode = !string.IsNullOrWhiteSpace(trackingCode) ? trackingCode : "Unknown";
+            Description = description;
+            Weight = weight;
+            DeliveryFee = deliveryFee;
+            Destination = destination;
+        }
+
+        #endregion
+
+        #region Methods 
+        public void UpdateDeliveryFee(decimal newFee)
+        {
+            DeliveryFee = newFee > 0 ? newFee : DeliveryFee;
+        }
+
+        public void PrintShipment()
+        {
+            Console.WriteLine($"The Tracking Code: {TrackingCode}");
+            Console.WriteLine($"Description: {Description}");
+            Console.WriteLine($"Weight: {Weight}");
+            Console.WriteLine($"Delivery Fee: {DeliveryFee}");
+            Console.WriteLine($"Estimated Cost: {EstimatedCost}");
+            Console.WriteLine($"Destination: {Destination.GetFullAddress()}");
+        }
+        #endregion
+    }
+}
