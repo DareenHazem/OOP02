509d141c08baacbce407e5a25f6661f3e6232377
DareenHazem <dareen.hazem2005@gmail.com>
2026-08-06 03:52:56 +0300

    Question 2- Create Three Shipment Types- StandardShipment

---
+++ OOP02/StandardShipment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP02
+{
+    internal class StandardShipment:Shipment
+    {
+
+        #region Constructors
+        public StandardShipment(string trackingCode) :base(trackingCode)
+        {
+
+        }
+
+        public StandardShipment(string trackingCode, string description, double weight, decimal deliveryFee, DeliveryAddress destination) : base(trackingCode, description , weight, deliveryFee, destination)
+        {
+
+        }
+        #endregion
+
+
+    }
+}
