3a4b220cbda59cddeeda6d476c4c01ef132d6e13
DareenHazem <dareen.hazem2005@gmail.com>
2026-08-06 05:51:22 +0300

    Question 3 -  DeliveryCenter Class extand

--- OOP02/DeliveryCenter.cs
+++ OOP02/DeliveryCenter.cs
@@ -7,6 +7,7 @@ namespace OOP02
     internal class DeliveryCenter
     {
         Shipment[] array ;
+        String CenterName;
 
         #region Properties 
         public Shipment this[int index] {
@@ -60,11 +61,39 @@ namespace OOP02
             return false;
         }
 
+        public bool RemoveShipment(string trackingCode)
+        {
+            if ( (array == null) || this[trackingCode] == null ) 
+            {
+                return false;
+            }
+            else
+            {
+                int index = Array.IndexOf(array, this[trackingCode]);
+                for (int i = index; i < array.Length - 1; i++)
+                {
+                    array[i] = array[i + 1];
+                }
+                array[array.Length-1] = null; 
+                return true;
+            }
+
+        }
+
+        public void PrintAllShipments()
+        {
+            foreach (Shipment shipment in array)
+            {
+                shipment.PrintShipment();
+            }
+        }
+
+
         #endregion
 
         public DeliveryCenter()
         {
-            array = new Shipment[10];
+            array = new Shipment[20];
         }
     }
 }
