58ed0bc708ed45d553dc219e1e1f30340bf11f44
DareenHazem <dareen.hazem2005@gmail.com>
2026-08-07 16:31:36 +0300

    Assignment 3 - (Part 02 -Practical) -(5 - Class Relationships)

---
+++ OOP02/Driver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP02
+{
+    internal class Driver
+    {
+        public int DriverId { get; set; }
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public Driver(int id, string name, string num)
+        {
+            DriverId = id;
+            FullName = name;
+            PhoneNumber = num;
+        }
+    }
+}
