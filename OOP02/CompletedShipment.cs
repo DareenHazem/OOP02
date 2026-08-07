1f9857f20fdd22534d9ee7631ea2e81994c6db70
DareenHazem <dareen.hazem2005@gmail.com>
2026-08-07 16:40:55 +0300

    Assignment 3 - (Part 02 -Practical) -(8 - Sealed Class)

---
+++ OOP02/CompletedShipment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP02
+{
+    internal sealed class CompletedShipment:Shipment
+    {
+        #region Constructors
+        public CompletedShipment(string trackingCode) : base(trackingCode)
+        {
+
+        }
+
+        public CompletedShipment(string trackingCode, string description, double weight, decimal deliveryFee, DeliveryAddress destination) : base(trackingCode, description, weight, deliveryFee, destination)
+        {
+
+        } 
+        #endregion
+    }
+}
